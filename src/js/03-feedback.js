588fddd1b99f86b290b4c8113aad9ebb7d2d76b6
Alex <Stolyarov9221@gmail.com>
2023-03-30 20:49:48 +0300

    change

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,38 +2,36 @@ import '../css/common.css';
 import '../css/03-feedback.css';
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'feedback-form-state';
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-  input: document.querySelector('input'),
-};
-const formData = {};
+const form = document.querySelector('.feedback-form');
 
-populateTextarea();
+form.addEventListener('input', throttle(formDataInput, 500));
+form.addEventListener('submit', formSubmit);
 
-refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+const KEY_STORAGE = 'feedback-form-state';
+let savedSettings = JSON.parse(localStorage.getItem(KEY_STORAGE)) || {};
 
-refs.form.addEventListener('submit', e => {
-  e.preventDefault();
-  e.currentTarget.reset();
-  const objData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  localStorage.removeItem(STORAGE_KEY);
-});
-
-function onTextareaInput(e) {
-  formData[e.target.name] = e.target.value;
-  const stringifiedData = JSON.stringify(formData);
-  localStorage.setItem(STORAGE_KEY, stringifiedData);
+function formDataInput(event) {
+  savedSettings[event.target.name] = event.target.value;
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(savedSettings));
 }
 
-function populateTextarea() {
-  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+function formSubmit(event) {
+  event.preventDefault();
 
-  if (savedMessage === null) {
-    //console.log(savedMessage);
-    return;
+  if (form.email.value === '' || form.message.value === '') {
+    return alert('Please fill out the form !');
   }
-  refs.textarea.value = savedMessage['message'] || '';
-  refs.input.value = savedMessage['email'] || '';
+
+  const saveData = localStorage.getItem(KEY_STORAGE);
+  console.log(JSON.parse(saveData));
+
+  event.currentTarget.reset();
+  localStorage.removeItem(KEY_STORAGE);
+  savedSettings = {};
 }
+
+function readyUserData() {
+  form.email.value = savedSettings.email || '';
+  form.message.value = savedSettings.message || '';
+}
+readyUserData();
